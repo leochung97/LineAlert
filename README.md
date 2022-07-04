a0b098a870d9109dc24465326f6834cdce2ed875
nikumar1206 <nikumar1206@gmail.com>
2022-07-03 22:31:25 -0400

    small updates to app.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LineAlert
+
+username: admin
+password: THgIXY5w72U7g5N1
+
+mongodb+srv://admin:<password>@linealert.yeohp.mongodb.net/?retryWrites=true&w=majorityTHgIXY5w72U7g5N1
+THgIXY5w72U7g5N1
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const expressListRoutes = require("express-list-routes");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+expressListRoutes(app, { prefix: "/api" });
