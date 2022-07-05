b7d2dfc17ee2da9668b9a54771da768ecf00f13a
Vivian Chen <vnchen92@gmail.com>
2022-07-04 23:29:25 -0400

    drafted alert api utils, actions and reducer

---
+++ frontend/src/reducers/alert_reducer.js
@@ -0,0 +1,26 @@
+// import { 
+//     RECEIVE_ALERTS, 
+//     RECEIVE_ALERT, 
+//     REMOVE_ALERT
+// } from '../actions/alert_actions';
+
+// const initialState = {
+
+// }
+
+// const AlertsReducer = (state = initialState, action) => {
+//     Object.freeze(state);
+//     let newState = {...state};
+//     switch(action.type) {
+//         case RECEIVE_ALERTS:
+
+//         case RECEIVE_ALERT:
+
+//         case REMOVE_ALERT:
+
+//         default:
+//             return state;
+//     }
+// }
+
+// export default AlertsReducer;
