5142d6c6b137f1a4b777072257e8519875bc995d
Vivian Chen <vnchen92@gmail.com>
2022-07-05 15:21:51 -0400

    Fixed utils for station and alerts

--- frontend/src/util/station_api_util.js
+++ frontend/src/util/station_api_util.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const fetchStation = stationId => {
-    return axios.get(`/api/stations/${stationId}`);
+    return axios.get(`/api/stations/${stationId}`)
+        .then(res => {
+            return res.data
+        })
 }
