173d30645692c55e948c2ed5d49600ef84d5fab0
Vivian Chen <vnchen92@gmail.com>
2022-07-11 15:31:20 -0400

    api key hidden

--- routes/api/google.js
+++ routes/api/google.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+const googleUrl = `https://maps.googleapis.com/maps/api/js?key=`;
+const endingUrl = `&libraries=places`;
+
 router.get("/", (req, res) => {
   if (process.env.REACT_APP_MAP_KEY) {
-    res.json(process.env.REACT_APP_MAP_KEY);
+    res.json(googleUrl.concat(process.env.REACT_APP_MAP_KEY).concat(endingUrl));
   } else {
     return res.status(400).json("Api key is missing");
   }
