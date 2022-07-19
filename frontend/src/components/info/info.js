9baed96c394ce30943a9f4f61063fc576d4dfce5
Leo Chung <leo.chung97@gmail.com>
2022-07-19 16:41:31 -0400

    Corrected text on info modal

--- frontend/src/components/info/info.js
+++ frontend/src/components/info/info.js
@@ -20,7 +20,7 @@ export default function InfoModal(props) {
         <svg className='close-modal' height="15pt" viewBox="0 0 500 500" width="15pt" onClick={props.closeModal}>
           <path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0" />
         </svg>
-        <h1>Application Info</h1>
+        <h1>Info</h1>
         <div className="info-text">
           <div className='link-section'>
             <a href='https://github.com/leochung97/LineAlert' target="_blank" rel="noreferrer">
@@ -28,15 +28,15 @@ export default function InfoModal(props) {
             </a>
           </div>
           <div className='info-section'>
-            <h5>Sign up or Log In for the ability to alert others.</h5>
-            <p>Anonymous users will still have access to all other functionality.</p>
+            <h5>Sign up or Log In for the ability to create alerts for others.</h5>
+            <p>Anonymous users will still have access to all other functionalities.</p>
           </div>
           <div className='info-section'>
             <h5>Use the toggle button on the top left of the map to switch the transit layer on and off.</h5>
           </div>
           <div className='info-section'>
-            <h5>As alerts are created, color coded markers will pop up at each station where the hazard/activity is located.</h5>
-            <p>Each marker is clickable and a blurb will pop up to display the alert.</p>
+            <h5>As alerts are created, color coded markers will pop up at each station where the alert is located.</h5>
+            <p>Each marker on the map can be clicked to pop up a display of the alert details.</p>
           </div>
           <div className='info-section'>
             <h5>Find the safest and fastest route using our directions handler on the top right.</h5> 
@@ -44,7 +44,7 @@ export default function InfoModal(props) {
             <p>Keep clicking next if you would like to use the next best route.</p>
           </div>
           <div className='info-section'>
-            <h5>All alerts will be displayed on the bottom left in chronilogical order, as they are created.</h5>
+            <h5>All alerts will be displayed on the bottom right in chronological order.</h5>
           </div>
           <div className='info-section'>
             <h5>Stay safe and keep others safe!</h5>
