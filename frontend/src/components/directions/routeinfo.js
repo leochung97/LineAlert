36c8eae106c27b385b653e93f6932c6b4f232944
Leo Chung <leo.chung97@gmail.com>
2022-07-19 15:41:15 -0400

    Removed routes hazard tracker' gpom

--- frontend/src/components/directions/routeinfo.js
+++ frontend/src/components/directions/routeinfo.js
@@ -6,29 +6,25 @@ function RouteInfo({route}) {
             <h1 className="route-container-header">Fastest Route</h1>
             <div className="route-container-tripinfo">
                 <div className='arrival-duration-distance'>
-                <div className='fastest-route-info-container-third'>
-                    <h4 className='fastest-route-h4'>Duration</h4><div className='fastest-route-p'> {route.duration}</div>
-                    </div>
-                    <div className='fastest-route-info-container-third'>
-                        <h4 className='fastest-route-h4'>Arrival Time</h4><div className='fastest-route-p'> {route.arrivalTime}</div>
-                    </div>
-                    <div className='fastest-route-info-container-third-last'>
-                        <h4 className='fastest-route-h4'>Distance</h4><div className='fastest-route-p'>{route.distance}</div>
-                    </div>
-                </div>
-                <div className='fastest-route-info-container'>
-                    <h4 className='fastest-route-h4'>Directions</h4><div className='fastest-route-p'>{route.trains.map((train) => {
-                    return (
-                        <div key={Math.random()} className="route-container-step">
-                          <span>Take the {train.trainName} train {train.instructions.slice(6)} from the {train.departureStop} train station. Exit the train at the {train.arrivalStop} stop.</span>
-                        </div>
-                    )
-                })}
-                </div>
-                </div>
-                <div className='fastest-route-info-container'>
-                <h4 className='fastest-route-h4'>Hazards on Path</h4>
-                <p className='fastest-route-p'>Alert here!</p>
+                  <div className='fastest-route-info-container-third'>
+                      <h4 className='fastest-route-h4'>Duration</h4><div className='fastest-route-p'> {route.duration}</div>
+                      </div>
+                      <div className='fastest-route-info-container-third'>
+                          <h4 className='fastest-route-h4'>Arrival Time</h4><div className='fastest-route-p'> {route.arrivalTime}</div>
+                      </div>
+                      <div className='fastest-route-info-container-third-last'>
+                          <h4 className='fastest-route-h4'>Distance</h4><div className='fastest-route-p'>{route.distance}</div>
+                      </div>
+                  </div>
+                  <div className='fastest-route-info-container'>
+                      <h4 className='fastest-route-h4'>Directions</h4><div className='fastest-route-p'>{route.trains.map((train) => {
+                      return (
+                          <div key={Math.random()} className="route-container-step">
+                            <span>Take the {train.trainName} train {train.instructions.slice(6)} from the {train.departureStop} train station. Exit the train at the {train.arrivalStop} stop.</span>
+                          </div>
+                      )
+                  })}
+                  </div>
                 </div>
             </div>
         </div>
